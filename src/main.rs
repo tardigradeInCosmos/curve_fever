0bbc4c3f472588b52ff77ce9400bf1c13753292a
SeaLama <143707149+SeaLama@users.noreply.github.com>
2024-04-06 14:29:13 +0100

    initial module separation

--- src/main.rs
+++ src/main.rs
@@ -1,157 +1,163 @@
-use std::io::stdin;
-use terminal_size::{Width, Height, terminal_size};
 use rand::Rng;
 
-struct Steering {
-    left: String,
-    right :String
-}
-impl PartialEq for Steering {
-    fn eq(&self, other: &Self) -> bool {
-        self.left == other.left || self.left == other.right || self.right == other.right || self.right == other.left 
-    }
-}
-struct Player {
-    steering: Steering,
-    designate: String
-}
-impl PartialEq for Player {
-    fn eq(&self, other: &Self) -> bool {
-        self.steering == other.steering || self.designate == other.designate
-    }
-}
-struct Board {
-    w: u16,
-    h: u16
-}
-
 fn main() {
-    print_header();
-    let players = create_players();
-    print_players_choice(&players);
-    print_board();
+    view::print_header();
+    user_interaction::create_players();
+    view::print_board();
 }
 
 fn get_random_number_from_range(bound: u16) -> u16 {
     rand::thread_rng().gen_range(0..bound)
-
 }
 
-fn print_board() {
-    let board = get_terminal_size();
-    let rows = board.h;
-    let cols = board.w;
-    print_rectangle(cols, rows);
-}
+mod view {
+    use terminal_size::{Width, Height, terminal_size};
 
-fn print_rectangle(width: u16, height: u16) {
+    struct Board {
+        w: u16,
+        h: u16
+    }
 
-    for i in 2..=height {
-        let mut printable_row = String::new().to_owned();
-        for j in 1..=width {
-            let sign = match (i, j) {
-                (2, _n) => '_',
-                (_m, 1) => '|',
-                (_m, n) if n == width => '|',
-                (m, _n) if m == height => '_',
-                _ => ' '
-            };
-        printable_row.push(sign);
+   pub fn print_board() {
+        let board = get_terminal_size();
+        let rows = board.h;
+        let cols = board.w;
+        print_rectangle(cols, rows);
+    }
+
+    fn print_rectangle(width: u16, height: u16) {
+
+        for i in 2..=height {
+            let mut printable_row = String::new().to_owned();
+            for j in 1..=width {
+                let sign = match (i, j) {
+                    (2, _n) => '_',
+                    (_m, 1) => '|',
+                    (_m, n) if n == width => '|',
+                    (m, _n) if m == height => '_',
+                    _ => ' '
+                };
+            printable_row.push(sign);
+            }
+            println!("{}", printable_row);
         }
-        println!("{}", printable_row);
+    }
+
+    fn get_terminal_size() -> Board {
+        let size = terminal_size();
+        if let Some((Width(w), Height(h))) = size {
+            println!("Your terminal is {} cols wide and {} lines tall", w, h);
+            Board { w: w, h: h }
+        } else {
+            println!("Unable to get terminal size");
+            Board { w: 10, h: 10 }
+        }
+    }    
+
+    pub fn print_header() {
+        println!("*********************");
+        println!("*********************");
+        println!("**   curve fever   **");
+        println!("*********************");
+        println!("*********************");
     }
 }
 
-fn get_terminal_size() -> Board {
-    let size = terminal_size();
-    if let Some((Width(w), Height(h))) = size {
-        println!("Your terminal is {} cols wide and {} lines tall", w, h);
-        Board { w: w, h: h }
-    } else {
-        println!("Unable to get terminal size");
-        Board { w: 10, h: 10 }
+mod user_interaction {
+    use std::io::stdin;
+
+    struct Steering {
+        left: String,
+        right :String
     }
-}    
-
-fn print_players_choice(players: &Vec<Player>) {
-    for (index, player) in players.iter().enumerate() {
-        println!("player no. {} choose key `{}` for left and `{}` for right and `{}` to designate self", index+1, player.steering.left, player.steering.right, player.designate);
+    impl PartialEq for Steering {
+        fn eq(&self, other: &Self) -> bool {
+            self.left == other.left || self.left == other.right || self.right == other.right || self.right == other.left 
+        }
     }
-}
-
-fn create_players() -> Vec<Player> {
-
-    let players_number: u8 = get_players_number();
-
-    let mut players_steerings: Vec<Player> = Vec::new();
-
-    for i in 0..players_number {
-        println!("Set steering for {} player", i+1);
-        let steering = loop {
-            let steering = get_users_steering_charset();
-            if !is_duplicate(&players_steerings, &steering) { break steering }
-        };
-        players_steerings.push(steering);
+    pub struct Player {
+        steering: Steering,
+        designate: String
+    }
+    impl PartialEq for Player {
+        fn eq(&self, other: &Self) -> bool {
+            self.steering == other.steering || self.designate == other.designate
+        }
     }
 
-    players_steerings
-}
+    fn get_input(prompt: &str) -> String {
+        println!("{}", prompt);
+        let mut answer = String::new();
+        stdin()
+            .read_line(&mut answer)
+            .expect("Failed to read line");
 
-fn get_players_number() -> u8 {
-    let players_count = loop {
+         answer
+    }
+
+    fn get_players_number() -> u8 {
+        let players_count = loop {
    
-        let num = get_input("How many players there's going to be?");
+            let num = get_input("How many players there's going to be?");
 
-        let num: u8 = match num.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
+            let num: u8 = match num.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+            if num > 0 { break num };
+            continue;
         };
-         if num > 0 { break num };
-         continue;
-    };
-   players_count 
-}
-
-fn is_duplicate(holder: &Vec<Player>, new_val: &Player) -> bool {
-    let mut is_duplicated = false;
-    for holding in holder.iter() {
-        is_duplicated = holding == new_val; 
-        if is_duplicated  { break };
+        players_count 
     }
 
-    is_duplicated
-}
-
-fn get_users_steering_charset () -> Player {
-    let mut answer = String::new();
-    while answer.len() <= 2  {
-        answer = get_input("enter 3 signs - first 2 will be your left|right steering keys, latter will identify you on a board");
+    fn print_players_choice(players: &Vec<Player>) {
+        for (index, player) in players.iter().enumerate() {
+            println!("player no. {} choose key `{}` for left and `{}` for right and `{}` to designate self", index+1, player.steering.left, player.steering.right, player.designate);
+        }
     }
-    let left = String::from(&answer[0..1]);
-    let right = String::from(&answer[1..2]);
-    let user = String::from(&answer[2..3]);
 
-    Player {
-        steering: Steering { left: left, right: right },
-        designate: user
+     pub fn create_players() -> Vec<Player> {
+
+        let players_number: u8 = get_players_number();
+
+        let mut players_steerings: Vec<Player> = Vec::new();
+
+        for i in 0..players_number {
+            println!("Set steering for {} player", i+1);
+            let steering = loop {
+                let steering = get_users_steering_charset();
+                if !is_duplicate(&players_steerings, &steering) { break steering }
+            };
+            players_steerings.push(steering);
+        }
+        print_players_choice(&players_steerings);
+
+        players_steerings
+    }
+
+
+    fn is_duplicate(holder: &Vec<Player>, new_val: &Player) -> bool {
+        let mut is_duplicated = false;
+        for holding in holder.iter() {
+            is_duplicated = holding == new_val; 
+            if is_duplicated  { break };
+        }
+
+        is_duplicated
+    }
+
+    fn get_users_steering_charset () -> Player {
+        let mut answer = String::new();
+        while answer.len() <= 2  {
+            answer = get_input("enter 3 signs - first 2 will be your left|right steering keys, latter will identify you on a board");
+        }
+        let left = String::from(&answer[0..1]);
+        let right = String::from(&answer[1..2]);
+        let user = String::from(&answer[2..3]);
+
+        Player {
+            steering: Steering { left: left, right: right },
+            designate: user
+        }
     }
 }
-
-fn get_input(prompt: &str) -> String {
-    println!("{}", prompt);
-    let mut answer = String::new();
-    stdin()
-        .read_line(&mut answer)
-        .expect("Failed to read line");
-
-    answer
-}
-
-fn print_header() {
-    println!("*********************");
-    println!("*********************");
-    println!("**   curve fever   **");
-    println!("*********************");
-    println!("*********************");
-}
-
